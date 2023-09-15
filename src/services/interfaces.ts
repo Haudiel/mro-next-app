a068251415143e6fc5c4727f7cf63c9a7de37e98
Haudielx <l19330632@hermosillo.tecnm.mx>
2023-09-15 13:44:54 -0700

    cambios

--- src/services/interfaces.ts
+++ src/services/interfaces.ts
@@ -204,8 +204,3 @@ export const Options = [
   { value: "14243", label: "JIMENEZ MARTINEZ, MARIA GUADALUPE" },
   { value: "13460", label: "HERNANDEZ SALCIDO, LUIS PEDRO" },
 ];
-
-export const Jefatura = [
-  { value: "7237", label: "ZAPIAIN SALAZAR, SILVIA LIZETH" },
-  { value: "14567", label: "URQUIJO CAJIGAS, MARIA GUADALUPE" },
-]
